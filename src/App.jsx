b49926451282ac59a283387f5dc7aa693d8c3f64
Adam Szymański <adamszymanski27@hotmail.com>
2023-01-24 20:48:12 +0100

    Add react router and basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './Components/GlobalStyles/GlobalStyles';
 import { darkTheme, baseTheme } from './assets/Theme/theme';
+import { AppLayout } from './Components/AppLayout/AppLayout';
+import { PageNotFound } from './Components/PageNotFound/PageNotFound';
 
 function App() {
   return (
     <ThemeProvider theme={{ ...darkTheme, ...baseTheme }}>
       <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
