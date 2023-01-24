588852711d927858edc7cf1704d9b3798d7139de
Adam Szymański <adamszymanski27@hotmail.com>
2023-01-24 20:56:14 +0100

    Create multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { GlobalStyles } from './Components/GlobalStyles/GlobalStyles';
 import { darkTheme, baseTheme } from './assets/Theme/theme';
 import { AppLayout } from './Components/AppLayout/AppLayout';
 import { PageNotFound } from './Components/PageNotFound/PageNotFound';
+import { Page } from './Components/Page/Page';
+import { Home } from './Components/Home/Home';
 
 function App() {
   return (
@@ -11,7 +13,10 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AppLayout />} />
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="page/:slug" elemement={<Page />} />
+          </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
