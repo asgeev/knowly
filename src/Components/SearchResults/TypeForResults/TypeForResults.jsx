e7baab5681397f75e5fa6fa089c94b3ab5f3fd96
Adam Szymański <adamszymanski27@hotmail.com>
2023-03-30 13:31:49 +0200

    Add better look for view when query is empty

--- src/Components/SearchResults/TypeForResults/TypeForResults.jsx
+++ src/Components/SearchResults/TypeForResults/TypeForResults.jsx
@@ -18,22 +18,17 @@ export const TipBox = styled.div`
     flex-direction: row;
     align-items: center;
     gap: 0.6rem;
-    margin-bottom: 1rem;
-
-    p {
-        margin: 0;
-        font-weight: ${({ theme }) => theme.font.weight.w500};
-    }
 `
-export const TextBox = styled.span`
+export const TextBoxWithBackground = styled.span`
     padding: 0 0.8rem;
     background-color: ${({ theme }) => theme.color.background200};
     color: ${({ theme }) => theme.color.secondaryText};
     border-radius: 0.3rem;
 `
 export const StyledP = styled.p`
-    font-size: ${({ theme }) => theme.font.size.small};
-    margin: 4rem 0 4rem 1rem;
+    /* font-size: ${({ theme }) => theme.font.size.small}; */
+    /* font-weight: 400; */
+    margin: 3rem 0 2rem 1rem;
     color: ${({ theme }) => theme.color.secondaryText};
 `
 
@@ -43,17 +38,18 @@ export const TypeForResults = ({ selectedSearchIndex }) => {
             <TipContainer>
                 <TipBox>
                     <MdOutlineLightbulb size="1.8rem" />
-                    <p>Podpowiedź</p>
+                    <p>
+                        Otwórz wyszukiwarkę szybciej naciskając klawisz{'  '}
+                        <TextBoxWithBackground>s</TextBoxWithBackground>
+                    </p>{' '}
                 </TipBox>
-                <p>
-                    Otwórz wyszukiwarkę szybciej naciskając klawisz{'  '}
-                    <TextBox>s</TextBox>
-                </p>
             </TipContainer>
             <Divider />
             <StyledP>
                 Wyszukujesz w kategorii:{' '}
-                <TextBox>{selectedSearchIndex.title}</TextBox>
+                <TextBoxWithBackground>
+                    {selectedSearchIndex.title}
+                </TextBoxWithBackground>
             </StyledP>
         </TypeForResultsContainer>
     )
