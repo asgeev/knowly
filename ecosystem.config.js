14d6833609c694169f284f388a82f232d9b4de2b
polishghost27 <80470876+polishghost27@users.noreply.github.com>
2023-04-06 14:27:17 +0200

    Update ecosystem.config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,5 @@ module.exports = {
         PM2_SERVE_PORT: '8081',
         PM2_SERVE_SPA: 'true',
         PM2_SERVE_HOMEPAGE: 'index.html',
-    },
-    exec_mode: 'cluster',
-    instances: 'max',
+    }
 }
