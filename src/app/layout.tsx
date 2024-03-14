52c4833740424aa21be2cb54c1de46f9f321cc55
asgeev <adamszymanski27@hotmail.com>
2024-03-14 10:09:06 +0100

    Add custom colors, add new components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,18 @@
+import { Poppins } from 'next/font/google'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import './globals.css'
+
 export const metadata = {
     title: 'Knowly',
     description: 'Portal knowly',
 }
+
+const poppins = Poppins({
+    weight: '400',
+    variable: '--font-poppins',
+    subsets: ['latin'],
+})
 
 export default function RootLayout({
     children,
@@ -9,8 +20,12 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="pl">
+            <body className={`${poppins.className}`}>
+                <Header />
+                {children}
+                <Footer />
+            </body>
         </html>
     )
 }
