f1750a5029e5258323b49e3b39a88dc0d3228f5d
asgeev <adamszymanski27@hotmail.com>
2024-03-25 11:37:29 +0100

    Add styles to meilisearch modal

--- src/components/organisms/Meilisearch.tsx
+++ src/components/organisms/Meilisearch.tsx
@@ -1,5 +1,15 @@
 import React, { useRef } from 'react'
 import { SearchButton } from '../atoms/SearchButton'
+import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
+
+import {
+    InstantSearch,
+    SearchBox,
+    Highlight,
+    Index,
+    Hits,
+    PoweredBy,
+} from 'react-instantsearch'
 
 export const Meilisearch = () => {
     const searchDialogRef = useRef<HTMLDialogElement>(null)
@@ -13,6 +23,16 @@ export const Meilisearch = () => {
             searchDialogRef?.current?.close()
         }
     }
+
+    const { searchClient, setMeiliSearchParams } = instantMeiliSearch(
+        'http://localhost:7700', // Host
+        'YSUrA12HFilbkxkpCE72UWJWuvB79EZyZlbQXsKObPc', // API key
+        {
+            placeholderSearch: false,
+            primaryKey: 'id',
+        }
+    )
+
     return (
         <>
             <SearchButton openMeilisearch={openMeilisearch} />
@@ -20,10 +40,76 @@ export const Meilisearch = () => {
                 ref={searchDialogRef}
                 onClick={closeMeilisearch}
                 //TODO: add colors for light theme
-                className="my-0 top-32 w-10/12 max-w-3xl backdrop:bg-backdrop backdrop:backdrop-blur-sm bg-inherit"
+                className="my-0 top-32 w-11/12 max-w-3xl backdrop:bg-backdrop backdrop:backdrop-blur-sm bg-inherit"
             >
-                <div className="bg-secondary p-10 rounded-lg shadow-meilisearch "></div>
+                <div className="bg-secondary rounded-lg shadow-meilisearch">
+                    <InstantSearch
+                        searchClient={searchClient}
+                        indexName="number"
+                    >
+                        <div className="flex flex-row px-4 items-center gap-4 border-b border-color_2 ">
+                            <span className="material-symbols-outlined md-24 text-textSecondary">
+                                search
+                            </span>
+                            <SearchBox
+                                classNames={{
+                                    root: 'w-full ',
+                                    form: 'appearance-none ',
+                                    input: 'unset focus:outline-none bg-transparent h-14 w-full font-medium',
+                                    submitIcon: 'hidden',
+                                    resetIcon: 'hidden',
+                                }}
+                                placeholder="Wyszukaj numeru telefonu"
+                            />
+                            <button
+                                onClick={closeMeilisearch}
+                                className="apperance-none text-textSecondary text-[10px] font-semibold bg-primary  p-2 rounded-md hover:text-accent"
+                            >
+                                ESC
+                            </button>
+                        </div>
+                        <div>
+                            <p className="py-16 px-6 text-center text-textSecondary font-medium">
+                                Brak wyszukiwań
+                            </p>
+                            {/* <Index indexName="number">
+                                <p className="text-bold">Numer</p>
+                                <Hits hitComponent={Hit} />
+                            </Index>
+                            <Index indexName="navigation-item">
+                                <p className="text-bold">Baza wiedzy</p>
+                                <Hits hitComponent={Hit1} />
+                            </Index> */}
+                        </div>
+
+                        <div className="border-t border-color_2 h-14 flex justify-end items-center px-4 py-6">
+                            <PoweredBy
+                                classNames={{
+                                    logo: 'h-4 text-red-200',
+                                }}
+                            />
+                        </div>
+                    </InstantSearch>
+                </div>
             </dialog>
+        </>
+    )
+}
+
+const Hit = ({ hit }) => {
+    return (
+        <>
+            <Highlight attribute="employeeFirstName" hit={hit} />
+            <p>{hit.internalNumber}</p>
+        </>
+    )
+}
+
+const Hit1 = ({ hit }) => {
+    return (
+        <>
+            <Highlight attribute="path" hit={hit} />
+            <p>{hit.path}</p>
         </>
     )
 }
