36bc661a78e59ca980bcc5a10e398e7dcd9b574b
asgeev <adamszymanski27@hotmail.com>
2024-04-09 11:16:37 +0200

    Add fetching data, add tags, add date for doc page, change styles

--- src/app/baza-wiedzy/[slug]/page.tsx
+++ src/app/baza-wiedzy/[slug]/page.tsx
@@ -1,9 +1,67 @@
-const DocPage = () => {
+import { notFound } from 'next/navigation'
+import { Tag } from '../../../components/atoms/Tag'
+import { changeDate } from '../../../helpers/changeDate'
+
+const getDocPageContent = async (slug: string) => {
+    const response = await fetch(
+        `${process.env.STRAPI_URL}/api/slugify/slugs/page/${slug}?fields[0]=title&fields[1]=content&fields[2]=createdAt&fields[3]=publishedAt&fields[4]=updatedAt&populate[0]=tags`
+    )
+
+    if (!response.ok) {
+        notFound()
+    }
+    return response?.json()
+}
+
+const DocPage = async ({
+    params,
+}: {
+    params: {
+        slug: string
+    }
+}) => {
+    const { data } = await getDocPageContent(params.slug)
+
+    const pageData = data.attributes
+
+    const formattedDate = changeDate(pageData?.createdAt)
+
     return (
-        <div className="">
-            Doc content
-            <h1 id="test">test</h1>
-        </div>
+        <>
+            {pageData && (
+                <div className="">
+                    <article className="prose dark:prose-invert prose-gray">
+                        <div className="pb-6">
+                            <p className="mt-1.5 text-textSecondary">
+                                {formattedDate}
+                            </p>
+                            <h1 className="text-textPrimary ">
+                                {pageData?.title}
+                            </h1>
+                            {pageData.tags && (
+                                <div className="flex flex-wrap gap-2">
+                                    {pageData.tags?.data.map(
+                                        ({ id, attributes }) => {
+                                            return (
+                                                <Tag
+                                                    key={id}
+                                                    text={attributes.slug}
+                                                />
+                                            )
+                                        }
+                                    )}
+                                </div>
+                            )}
+                        </div>
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: pageData?.content,
+                            }}
+                        ></div>
+                    </article>
+                </div>
+            )}
+        </>
     )
 }
 
--- src/app/baza-wiedzy/page.tsx
+++ src/app/baza-wiedzy/page.tsx
@@ -1,7 +1,7 @@
 export default function Docs() {
     return (
         <article className="lg:mx-auto prose dark:prose-invert prose-gray w-full">
-            <h1>Lorem ipsum</h1>
+            <h2>Lorem ipsum</h2>
             <p>
                 What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
                 printing and typesetting industry. Lorem Ipsum has been the
@@ -25,75 +25,10 @@ export default function Docs() {
                 accident, sometimes on purpose (injected humour and the like).
                 🎉
             </p>
-            <p>
-                What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-                printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an
-                unknown printer took a galley of type and scrambled it to make a
-                type specimen book. It has survived not only five centuries, but
-                also the leap into electronic typesetting, remaining essentially
-                unchanged. It was popularised in the 1960s with the release of
-                Letraset sheets containing Lorem Ipsum passages, and more
-                recently with desktop publishing software like Aldus PageMaker
-                including versions of Lorem Ipsum. Why do we use it? It is a
-                long established fact that a reader will be distracted by the
-                readable content of a page when looking at its layout. The point
-                of using Lorem Ipsum is that it has a more-or-less normal
-                distribution of letters, as opposed to using 'Content here,
-                content here', making it look like readable English. Many
-                desktop publishing packages and web page editors now use Lorem
-                Ipsum as their default model text, and a search for 'lorem
-                ipsum' will uncover many web sites still in their infancy.
-                Various versions have evolved over the years, sometimes by
-                accident, sometimes on purpose (injected humour and the like).
-                🎉
-            </p>
-            <p>
-                What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-                printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an
-                unknown printer took a galley of type and scrambled it to make a
-                type specimen book. It has survived not only five centuries, but
-                also the leap into electronic typesetting, remaining essentially
-                unchanged. It was popularised in the 1960s with the release of
-                Letraset sheets containing Lorem Ipsum passages, and more
-                recently with desktop publishing software like Aldus PageMaker
-                including versions of Lorem Ipsum. Why do we use it? It is a
-                long established fact that a reader will be distracted by the
-                readable content of a page when looking at its layout. The point
-                of using Lorem Ipsum is that it has a more-or-less normal
-                distribution of letters, as opposed to using 'Content here,
-                content here', making it look like readable English. Many
-                desktop publishing packages and web page editors now use Lorem
-                Ipsum as their default model text, and a search for 'lorem
-                ipsum' will uncover many web sites still in their infancy.
-                Various versions have evolved over the years, sometimes by
-                accident, sometimes on purpose (injected humour and the like).
-                🎉
-            </p>
-            <p>
-                What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-                printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an
-                unknown printer took a galley of type and scrambled it to make a
-                type specimen book. It has survived not only five centuries, but
-                also the leap into electronic typesetting, remaining essentially
-                unchanged. It was popularised in the 1960s with the release of
-                Letraset sheets containing Lorem Ipsum passages, and more
-                recently with desktop publishing software like Aldus PageMaker
-                including versions of Lorem Ipsum. Why do we use it? It is a
-                long established fact that a reader will be distracted by the
-                readable content of a page when looking at its layout. The point
-                of using Lorem Ipsum is that it has a more-or-less normal
-                distribution of letters, as opposed to using 'Content here,
-                content here', making it look like readable English. Many
-                desktop publishing packages and web page editors now use Lorem
-                Ipsum as their default model text, and a search for 'lorem
-                ipsum' will uncover many web sites still in their infancy.
-                Various versions have evolved over the years, sometimes by
-                accident, sometimes on purpose (injected humour and the like).
-                🎉
-            </p>
+            <h2>Cyberbezpieczeństwo</h2>
+            <h3>Cyberbezpieczeństwo</h3>
+            <h4>Cyberbezpieczeństwo</h4>
+            <h5>Cyberbezpieczeństwo</h5>
         </article>
     )
 }
