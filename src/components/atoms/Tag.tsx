6dc1f787addb22b08f0dc02ce02120605d9c7ebf
asgeev <adamszymanski27@hotmail.com>
2024-04-10 12:46:57 +0200

    Change tag component size

--- src/components/atoms/Tag.tsx
+++ src/components/atoms/Tag.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const Tag = (props: Props) => {
     return (
-        <div className="bg-color_1 rounded-full px-5 py-2 hover:border-orange-500  whitespace-nowrap">
+        <div className="bg-color_1 rounded-full text-sm md:text-base px-5 py-2 hover:border-orange-500  whitespace-nowrap">
             <span>{props.text}</span>
         </div>
     )
