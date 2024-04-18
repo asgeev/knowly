cd58cb38a9b5dc47773a16ceb102634eb943e153
asgeev <adamszymanski27@hotmail.com>
2024-04-18 11:27:30 +0200

    Add styles for mobile devices for pinned item, change styles for showing pinned items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import { PinnedNewsItem } from '../components/molecules/PinnedNewsItem'
 export default function Home() {
     return (
         <div className="flex flex-col gap-20 md:gap-12 mt-10">
-            <div className="space-y-4">
-                <h1 className="text-xl">Przypięte</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div className="space-y-6 md:space-y-10">
+                <h1 className="font-bold text-2xl md:text-4xl">Przypięte</h1>
+                <div className="grid sm:grid-flow-col gap-4 overflow-y-auto py-4">
                     <PinnedNewsItem
                         href="/intranet/raporty"
                         title="Nowość! Strona raportów już istnieje!"
-                        coverUrl="http://localhost:1337/uploads/37c11901d47e22cb61587cb493ab4872_1bff35cb16.webp"
+                        coverUrl="http://localhost:1337/uploads/4aca9e73dfe46dbfbc4af8ec53f54d25_179b51d211.webp"
                         tag="Informatyka"
                     />
                     <PinnedNewsItem
@@ -28,7 +28,43 @@ export default function Home() {
                     <PinnedNewsItem
                         href="/raporty/sprawozdania"
                         title="Nowe rozwiązania informatyczne już niedługo"
-                        coverUrl="http://localhost:1337/uploads/Success_2418e558a5.png"
+                        coverUrl="http://localhost:1337/uploads/01893a9429f7588db8058ecb1439bb46_bdbca91336.webp"
+                        tag="Cyberbezpieczeństwo"
+                    />
+                    <PinnedNewsItem
+                        href="/raporty/sprawozdania"
+                        title="Nowe rozwiązania informatyczne już niedługo"
+                        coverUrl="http://localhost:1337/uploads/01893a9429f7588db8058ecb1439bb46_bdbca91336.webp"
+                        tag="Cyberbezpieczeństwo"
+                    />
+                    <PinnedNewsItem
+                        href="/raporty/sprawozdania"
+                        title="Nowe rozwiązania informatyczne już niedługo"
+                        coverUrl="http://localhost:1337/uploads/01893a9429f7588db8058ecb1439bb46_bdbca91336.webp"
+                        tag="Cyberbezpieczeństwo"
+                    />
+                    <PinnedNewsItem
+                        href="/raporty/sprawozdania"
+                        title="Nowe rozwiązania informatyczne już niedługo"
+                        coverUrl="http://localhost:1337/uploads/01893a9429f7588db8058ecb1439bb46_bdbca91336.webp"
+                        tag="Cyberbezpieczeństwo"
+                    />
+                    <PinnedNewsItem
+                        href="/raporty/sprawozdania"
+                        title="Nowe rozwiązania informatyczne już niedługo"
+                        coverUrl="http://localhost:1337/uploads/01893a9429f7588db8058ecb1439bb46_bdbca91336.webp"
+                        tag="Cyberbezpieczeństwo"
+                    />
+                    <PinnedNewsItem
+                        href="/raporty/sprawozdania"
+                        title="Nowe rozwiązania informatyczne już niedługo"
+                        coverUrl="http://localhost:1337/uploads/01893a9429f7588db8058ecb1439bb46_bdbca91336.webp"
+                        tag="Cyberbezpieczeństwo"
+                    />
+                    <PinnedNewsItem
+                        href="/raporty/sprawozdania"
+                        title="Nowe rozwiązania informatyczne już niedługo"
+                        coverUrl="http://localhost:1337/uploads/01893a9429f7588db8058ecb1439bb46_bdbca91336.webp"
                         tag="Cyberbezpieczeństwo"
                     />
                 </div>
