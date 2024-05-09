ad201c1b95178c4c2910282b0fd107bfd9ebb1db
asgeev <adamszymanski27@hotmail.com>
2024-05-09 11:01:16 +0200

    Add category and category color

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,13 +1,12 @@
 import { notFound } from 'next/navigation'
 import Image from 'next/image'
 import { changeDate } from '../../../helpers/changeDate'
+import Link from 'next/link'
 
 const fetchPost = async (slug: string) => {
     const response = await fetch(
         `http://localhost:1337/api/slugify/slugs/post/${slug}?fields[0]=title&fields[1]=content&fields[2]=publishedAt&populate[0]=category,cover`
     )
-
-    console.log(response)
     if (response.status === 404) {
         notFound()
     } else if (!response.ok) {
@@ -27,7 +26,8 @@ export default async function Post({ params }: { params: { slug: string } }) {
 
     const strapiUrl: string | undefined = process.env.STRAPI_URL
     const localDate: string = changeDate(publishedAt)
-    const coverUrl: string = `${strapiUrl}${cover?.data?.attributes.formats.medium.url}`
+    const coverUrl: string = `${strapiUrl}${cover?.data?.attributes?.url}`
+    const categoryData: string = category?.data?.attributes
 
     return (
         <>
@@ -42,12 +42,24 @@ export default async function Post({ params }: { params: { slug: string } }) {
                             objectPosition="center center"
                         />
                     </div>
-                    <article className="prose max-w-none lg:prose-xl prose-img:rounded-xl dark:prose-invert prose-gray px-4">
-                        <p className="text-textSecondary text-base font-semibold">
-                            {localDate}
-                        </p>
+                    <article className="prose max-w-none lg:prose-xl prose-img:rounded-xl dark:prose-invert prose-gray px-6">
+                        <div className="flex gap-5 items-baseline">
+                            <Link
+                                href={`/kategoria/${categoryData?.slug}`}
+                                className="text-textSecondary font-extrabold hover:text-accent no-underline"
+                            >
+                                {categoryData?.name}
+                            </Link>
+                            <p className="text-textSecondary text-base font-semibold ">
+                                {localDate}
+                            </p>
+                        </div>
                         <div>
                             <h1>{title}</h1>
+                            <span
+                                className="w-14 h-2 rounded-lg block my-10"
+                                style={{ backgroundColor: categoryData?.color }}
+                            ></span>
 
                             {/* {tags && (
                                <div className="flex flex-row gap-2">
--- src/app/post/layout.tsx
+++ src/app/post/layout.tsx
@@ -6,7 +6,9 @@ export default function PostLayout({
     return (
         <div className="container mx-auto">
             <div className="lg:grid grid-cols-12 gap-5 mt-10">
-                <div className="col-span-8 bg-secondary">{children}</div>
+                <div className="col-span-8 bg-secondary rounded-b-lg">
+                    {children}
+                </div>
                 <div className="max-lg:hidden col-span-4">asfasfasf</div>
             </div>
         </div>
