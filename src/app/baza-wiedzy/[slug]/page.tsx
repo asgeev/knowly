b8eb5403e08a421653908fc94f82a58f685b44eb
asgeev <adamszymanski27@hotmail.com>
2024-05-16 11:46:50 +0200

    Add publishAt and updateAt elements for docs page

--- src/app/baza-wiedzy/[slug]/page.tsx
+++ src/app/baza-wiedzy/[slug]/page.tsx
@@ -13,23 +13,37 @@ const DocPage = async ({
 
     const pageData = data.attributes
 
-    const formattedDate = changeDate(pageData?.createdAt)
+    const publishedAt = changeDate(pageData?.publishedAt)
+    const updatedAt = changeDate(pageData?.updatedAt)
 
     return (
-        <>
+        <div>
             {pageData && (
-                <div className="">
+                <div>
                     <article className="prose max-w-none lg:prose-lg prose-imp:rounded-xl dark:prose-invert prose-gray px-4 lg:px-2">
-                        <div className="pb-6">
-                            <p className="mt-1.5 text-textSecondary">
-                                {formattedDate}
-                            </p>
-                            <h1 className="text-textPrimary ">
-                                {pageData?.title}
-                            </h1>
-                            {pageData.tags && (
+                        <div className="pb-5 lg:pt-8">
+                            <h1>{pageData?.title}</h1>
+                            <div className="not-prose flex gap-7 text-base text-textSecondary">
+                                {publishedAt && (
+                                    <div className="flex items-center gap-2">
+                                        <span className="material-symbols-outlined">
+                                            calendar_month
+                                        </span>
+                                        <p>Publikacja: {publishedAt}</p>
+                                    </div>
+                                )}
+                                {updatedAt && (
+                                    <div className="flex items-center gap-2">
+                                        <span className="material-symbols-outlined">
+                                            published_with_changes
+                                        </span>
+                                        <p>Aktualizacja: {updatedAt}</p>
+                                    </div>
+                                )}
+                            </div>
+                            {pageData?.tags && (
                                 <div className="flex flex-wrap gap-2">
-                                    {pageData.tags?.data.map(
+                                    {pageData?.tags?.data.map(
                                         ({ id, attributes }) => {
                                             return (
                                                 <Tag
@@ -50,7 +64,7 @@ const DocPage = async ({
                     </article>
                 </div>
             )}
-        </>
+        </div>
     )
 }
 
