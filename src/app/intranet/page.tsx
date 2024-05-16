1f14068599ead7077313c065cba565ed59f35d47
asgeev <adamszymanski27@hotmail.com>
2024-05-16 15:36:12 +0200

    Add in progress to intranet main page

--- src/app/intranet/page.tsx
+++ src/app/intranet/page.tsx
@@ -1,5 +1,27 @@
+import Link from 'next/link'
+
 const Intranet = () => {
-    return <div>Intranet page</div>
+    return (
+        <div className="container mx-auto">
+            <div className="flex flex-col  items-center justify-center h-[400px]">
+                <h1 className="text-6xl font-bold">Strona w budowie</h1>
+                <p className="text-textSecondary mt-5">
+                    Jeszcze nie jesteśmy gotowi, daj nam trochę czasu!
+                </p>
+                <Link
+                    href={'/'}
+                    className="group mt-8 text-white bg-accent px-10 py-3 rounded-md w-48  "
+                >
+                    <div className="flex gap-2 group-hover:gap-4 transition-all items-center justify-center">
+                        <p>Główna</p>
+                        <span className="material-symbols-outlined md-18">
+                            arrow_forward
+                        </span>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default Intranet
