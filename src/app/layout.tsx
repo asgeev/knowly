7f14e33b8433c3158c67c1f15e1a48d574a8f2f6
asgeev <adamszymanski27@hotmail.com>
2024-05-24 13:18:44 +0200

    Fix removed links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,10 @@ export default function RootLayout({
                     href="https://fonts.googleapis.com/icon?family=Material+Icons"
                     rel="stylesheet"
                 />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+                    rel="stylesheet"
+                />
                 <title>Knowly</title>
             </head>
             <body
