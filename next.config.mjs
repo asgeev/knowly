62b332be879fde859d0d17a82f8dc51424d9c2b2
asgeev <adamszymanski27@hotmail.com>
2024-05-30 20:53:09 +0200

    Update next js config, update .env.example

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
         formats: ['image/avif', 'image/webp'],
         remotePatterns: [
             {
-                protocol: 'https',
+                protocol: 'http',
                 hostname: "**",
             },
         ],
