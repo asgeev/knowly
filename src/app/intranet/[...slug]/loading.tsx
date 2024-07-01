93944f69c7be6faa7c9db51f07cc7674b2a08e6d
asgeev <adamszymanski27@hotmail.com>
2024-07-01 15:02:34 +0200

    Add loading to intranet page, suspense fast links, update intranet prose styles

---
+++ src/app/intranet/[...slug]/loading.tsx
@@ -0,0 +1,21 @@
+export default function Loading() {
+    return (
+        <div className="bg-secondary px-6 rounded-lg w-full py-20">
+            <div className="lg:mt-4">
+                <div className="h-12 w-1/2 bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg mb-10"></div>
+                <div className="h-6 w-1/3 bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg mb-16"></div>
+                <div className="space-y-6">
+                    <div className="h-6 w-full bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg"></div>
+                    <div className="h-6 w-full bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg"></div>
+                    <div className="h-6 w-full bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg"></div>
+                    <div className="h-6 w-full bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg"></div>
+                    <div className="h-6 w-full bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg"></div>
+                    <div className="h-6 w-full bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg"></div>
+                    <div className="h-6 w-full bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg"></div>
+                    <div className="h-6 w-full bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg"></div>
+                    <div className="h-6 w-1/3 bg-neutral-200 dark:bg-neutral-800 animate-pulse rounded-lg"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
